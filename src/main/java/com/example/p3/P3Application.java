7fb01507306284e55d61ed20df9bcd26e5b39a53
8ruun <mbja24@student.aau.dk>
2025-10-13 14:47:40 +0200

    JsonParse is implemented

--- src/main/java/com/example/p3/P3Application.java
+++ src/main/java/com/example/p3/P3Application.java
@@ -1,7 +1,10 @@
 package com.example.p3;
 
+import com.example.p3.service.LinkService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class P3Application {
@@ -9,6 +12,13 @@ public class P3Application {
     public static void main(String[] args) {
 
         SpringApplication.run(P3Application.class, args);
+
+
+        LinkService l = new LinkService();
+
+
+        l.JsonParser();
+
 
     }
 
