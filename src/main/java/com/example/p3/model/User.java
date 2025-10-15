26949479ccb20cc15726d6bf491dbe05e8e23340
Katrine123 <113664220+Katrine123@users.noreply.github.com>
2025-10-15 13:22:57 +0200

    add user class

---
+++ src/main/java/com/example/p3/model/User.java
@@ -0,0 +1,15 @@
+package com.example.p3.model;
+
+public class User {
+    private long id;
+    private String initials;
+    private String name;
+    private String email;
+    private Department department;
+    public enum Department {
+        DEVOPS, FRONTEND, GAMES, PAYMENTS, PLAYERS, PROMOTIONS
+    }
+
+    //Favourites
+    //Admin
+}
