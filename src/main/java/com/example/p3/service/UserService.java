9f184304d980b5fd8a5b56ad16ac83bf9cefb248
Asta Krogsgaard Jensen <akje24@student.aau.dk>
2025-10-15 14:04:18 +0200

    Json fil med mockdata + virker ikke

---
+++ src/main/java/com/example/p3/service/UserService.java
@@ -0,0 +1,50 @@
+package com.example.p3.service;
+
+import com.example.p3.dtos.UserDto;
+import com.example.p3.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class UserService {
+    private final Map<Long, User> inMemoryDbUser = new ConcurrentHashMap<>();
+
+    static long counter = 0;
+    public long useCounter() {
+        return ++counter;
+    }
+
+    @PostConstruct
+    public void seedData() {
+        JsonParserUser("src/main/resources/static/User_MockData.json");
+        // --- Mock data for development ---
+    }
+
+
+    public void JsonParserUser(String src) {
+        //https://fasterxml.github.io/jackson-databind/javadoc/2.7/com/fasterxml/jackson/databind/ObjectMapper.html
+        ObjectMapper mapper = new ObjectMapper();
+
+        //We are creating an empty list to fill up the with the json data
+        User[] users;
+
+        // I needed to make a try/catch otherwise it complained.
+        try {
+            //Reads the file that is provided and fits it to how tool looks
+            users = mapper.readValue(new File(src), User[].class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //We are posting all the elements to the DB.
+        for (User user : users) {
+            inMemoryDbUser.put(useCounter(), user);
+        }
+    }
+}
