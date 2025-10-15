c35a33aa10b035d50a472f6f68aaba295e487fcf
Asta Krogsgaard Jensen <akje24@student.aau.dk>
2025-10-15 14:46:50 +0200

    Dto næsten done

--- src/main/java/com/example/p3/dtos/UserDto.java
+++ src/main/java/com/example/p3/dtos/UserDto.java
@@ -1,4 +1,25 @@
 package com.example.p3.dtos;
 
+import com.example.p3.model.User;
+
 public class UserDto {
+    public User createUser(String spaceId,
+                           String id,
+                           String initials,
+                           String name,
+                           String email,
+                           User.Department[] department)
+
+    {
+        User createdUser = new User();
+        createdUser.setId(useCounter());
+        createdUser.setName(name);
+        createdUser.setInitials(initials);
+        createdUser.setEmail(email);
+        createdUser.setDepartment(department);
+
+        // Store the object createdtool in the Hash map (inMemoryDb), and use its ID (createdtool.getId()) as the key.
+        inMemoryDb.put(createdUser.getId(), createdUser);
+        return createdUser;
+    }
 }
