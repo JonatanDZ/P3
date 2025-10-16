76a8ec4ab9bfbac4efda401b915f128e223ecfc4
Asta Krogsgaard Jensen <akje24@student.aau.dk>
2025-10-16 12:43:50 +0200

    UserController Oprettet

--- src/main/java/com/example/p3/controller/ObjectiveController.java
+++ src/main/java/com/example/p3/controller/ObjectiveController.java
@@ -1,22 +1,16 @@
 package com.example.p3.controller;
 
-
 import com.example.p3.dtos.ToolDto;
-import com.example.p3.dtos.UserDto;
 import com.example.p3.model.Tool;
 import com.example.p3.service.ToolService;
-import com.example.p3.service.UserService;
 import org.springframework.http.ResponseEntity;
 
 import org.springframework.web.bind.annotation.*;
-
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 import java.util.List;
 
@@ -25,20 +19,11 @@ import java.util.List;
 @RequestMapping()
 public class ObjectiveController {
     private final ToolService toolService;
-    private final UserService userService;
 
-    public ObjectiveController(ToolService toolService, UserService userService) {
+    public ObjectiveController(ToolService toolService) {
         this.toolService = toolService;
-        this.userService = userService;
     }
 
-    @GetMapping("/getUsers")
-    public ResponseEntity<List<UserDto>> getAllUsers(){
-        List<UserDto> list = userService.getAllUsers().values().stream()
-                .map(UserDto::new)
-                .toList();
-        return ResponseEntity.ok(list);
-    }
 
 
 
@@ -92,7 +77,6 @@ public class ObjectiveController {
         if (tool ==null){
             return ResponseEntity.badRequest().build();
         }
-
 
         Tool createdTool = toolService.createTool(
                 null,
