b9d4d753949271afc2e5ab73cea5a5e85b4e6d3a
Asta Krogsgaard Jensen <akje24@student.aau.dk>
2025-10-16 12:49:05 +0200

    Update UserController.java

--- src/main/java/com/example/p3/controller/UserController.java
+++ src/main/java/com/example/p3/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.example.p3.controller;
 
 import com.example.p3.dtos.UserDto;
+import com.example.p3.model.User;
 import com.example.p3.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,23 @@ public class UserController {
     @GetMapping("/getUsers")
     public ResponseEntity<List<UserDto>> getAllUsers(){
         List<UserDto> list = userService.getAllUsers().values().stream()
+                .map(UserDto::new)
+                .toList();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/getUsers/{department}")
+    public ResponseEntity<List<UserDto>> getAllUsersByDepartment(@PathVariable User.Department department) {
+        List<UserDto> list = userService.getAllUsersByDepartment(department).values().stream()
+                .map(UserDto::new)
+                .toList();
+        return ResponseEntity.ok(list);
+    }
+
+    //Call "getUserById" which selects the user according to the id (skal være initialer) in the URL
+    @GetMapping("/getUser/{id}")
+    public ResponseEntity<List<UserDto>> getUserById(@PathVariable long id) {
+        List<UserDto> list = userService.getUserById(id).values().stream()
                 .map(UserDto::new)
                 .toList();
         return ResponseEntity.ok(list);
