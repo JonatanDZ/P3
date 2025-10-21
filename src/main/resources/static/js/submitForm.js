08e35a1c4e21b905e810cc299ec41e79a457ccf2
8ruun <mbja24@student.aau.dk>
2025-10-21 14:27:02 +0200

    DynamicLinkAdded

--- src/main/resources/static/js/submitForm.js
+++ src/main/resources/static/js/submitForm.js
@@ -4,12 +4,12 @@ import {MakeToolJSON} from "./fetchTool.js";
 export function submitForm(){
     console.log("Jeg bliver kaldt");
     const name = document.querySelector("#toolName").value;
-    const url = document.querySelector("#toolURL").value;
+    const dynamic = document.querySelector('#isDynamic').checked;
+    const url = getURLValue(dynamic);
     const tags = document.querySelector("#tags").value.split(",")
         .map(tag => tag.trim())
         .filter(tag => tag !== "");
-    let dynamic = document.querySelector('#isDynamic').checked;
-    let stages = Array.from(document.querySelectorAll('.stagesChecks:checked')).map(cb => cb.value);
+    const stages = Array.from(document.querySelectorAll('.stagesChecks:checked')).map(cb => cb.value);
     const departments = Array.from(document.querySelectorAll('.departmentsChecks:checked')).map(cb => cb.value);
     const jurisdictions = Array.from(document.querySelectorAll('.jurisdictionsChecks:checked')).map(cb => cb.value);
 
@@ -20,4 +20,16 @@ export function submitForm(){
     setTimeout(() => {
         window.location.reload();
     }, 100);
+}
+
+function getURLValue(dynamic){
+    const url1 = document.querySelector("#toolURL1").value.toString();
+    const user = document.querySelector("#toolUser").textContent;
+    const url2 = document.querySelector("#toolURL2").value.toString();
+
+    if (dynamic){
+        return url1 + user + url2;
+    } else {
+        return url1;
+    }
 }
--- src/main/resources/static/js/toggleForm.js
+++ src/main/resources/static/js/toggleForm.js
@@ -9,3 +9,19 @@ export function toggleForm(formIsShown){
     }
     return formIsShown
 }
+
+export function displayURLbar(dynamicCheck){
+    const toolURLInput1 = document.querySelector("#toolURL1");
+    const toolURLInput2 = document.querySelector("#toolURL2");
+    const user = document.querySelector("#toolUser");
+    if (dynamicCheck){
+        toolURLInput2.style.display = "inline-block";
+        user.style.display = "inline-block"
+        toolURLInput1.placeholder = "Enter first part of URL";
+    } else {
+        toolURLInput2.style.display = "none";
+        user.style.display = "none"
+        toolURLInput1.placeholder = "Enter URL";
+    }
+    console.log(toolURLInput2);
+}
