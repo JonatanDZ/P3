90aaa86c5a4f502658d39c689ac9f9b9918308e0
Asta Krogsgaard Jensen <akje24@student.aau.dk>
2025-10-21 17:43:23 +0200

    Makværk

--- src/test/java/com/example/p3/P3ApplicationTests.java
+++ src/test/java/com/example/p3/P3ApplicationTests.java
@@ -1,6 +1,6 @@
 package com.example.p3;
 
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,21 +9,54 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class P3ApplicationTests {
     @Autowired
-    private MockMvc mockMvc;
+    private ObjectMapper objectMapper;
+
+    private final RestTemplate restTemplate = new  RestTemplate();
+
+
     @Test
-    public void testHelloEndpoint() throws Exception {
-        mockMvc.perform(get("http://localhost:8080/user/getAll"))
-                .andExpect(status().isOk());
-                //.andExpect(content().string("Hello, World!"));
+    void testGetUserById() throws Exception {
+        String url = "http://localhost:8080/getUsers/id/15";
+
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+
+        //assertThat(response.getHeaders().getContentType().toString()).contains("application/json");
+
+        String expectedJson = """
+        [{"id":15,"initials":"msit","name":"Micky Sitwell","email":"msit@cego.dk","department":"PAYMENTS", "admin":true}]
+        """;
+
+        assertThat(objectMapper.readTree(response.getBody())).isEqualTo((objectMapper.readTree(expectedJson)));
     }
+
+//    @Autowired
+//    private MockMvc mockMvc;
+////    @Test
+////    public void testGetAllEndpoint() throws Exception {
+////        mockMvc.perform(get("/getUsers"))
+////                .andExpect(status().isOk());
+////                //.andExpect(content().string("Hello, World!"));
+////    }
+//
+//    @Test
+//    public void testIdEndpoint() throws Exception {
+//        mockMvc.perform(get("/getUsers/id/2"))
+//                .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.id").exists());
+//    }
 
 }
