b11770d7bce78239a2a3c9f02b62446395af41d1
8ruun <mbja24@student.aau.dk>
2025-10-21 15:16:10 +0200

    Test for DynamicLink

--- src/test/JavaScript/addToolForm/submitForm.test.js
+++ src/test/JavaScript/addToolForm/submitForm.test.js
@@ -13,7 +13,9 @@ describe('submitForm', () => {
         <div id="addToolDiv" style="display:none;"></div>
         <button class="toggleBtn"></button>
         <input id="toolName" value="Test Tool"/>
-        <input id="toolURL1" value="http://example.com"/>
+        <input id="toolURL1" value="http://example."/>
+        <b id="toolUser">USER</b>
+        <input id="toolURL2" value=".com"/>
         <input id="tags" value="tag1, tag2"/>
         <input type="checkbox" id="isDynamic"/>
         <div id="departmentsInput"></div>
@@ -26,7 +28,7 @@ describe('submitForm', () => {
         <button id="submitBtn"></button>
     `;
     });
-    test('submit form', async () => {
+    test('submit form non dynamic link', async () => {
 
 
         mockResponse = {
@@ -40,19 +42,41 @@ describe('submitForm', () => {
         expect(global.fetch).toHaveBeenCalled();
 
         const [url, options] = global.fetch.mock.calls[0]; // first call
-        console.log("Options:", options);
 
         const body = JSON.parse(options.body);
-        console.log(body.name)
+
         expect(body.name).toBe('Test Tool');
-        expect(body.url).toBe('http://example.com');
+        expect(body.url).toBe('http://example.');
         expect(body.tags).toEqual(['tag1', 'tag2']);
         expect(body.dynamic).toBe(false);
         expect(body.departments).toEqual(['DEPT1']);
         expect(body.jurisdictions).toEqual(['DK']);
         expect(body.stages).toEqual(['STAGE1']);
+    })
+    test('submit form dynamic link', async () => {
 
 
+        mockResponse = {
+            ok: true,
+            json: async () => ({ success: true }),
+        }
+        document.querySelector("#isDynamic").checked = true;
 
+        global.fetch.mockResolvedValue(mockResponse);
+        await submitForm();
+
+        expect(global.fetch).toHaveBeenCalled();
+
+        const [url, options] = global.fetch.mock.calls[0]; // first call
+
+        const body = JSON.parse(options.body);
+
+        expect(body.name).toBe('Test Tool');
+        expect(body.url).toBe('http://example.USER.com');
+        expect(body.tags).toEqual(['tag1', 'tag2']);
+        expect(body.dynamic).toBe(true);
+        expect(body.departments).toEqual(['DEPT1']);
+        expect(body.jurisdictions).toEqual(['DK']);
+        expect(body.stages).toEqual(['STAGE1']);
     })
 });
