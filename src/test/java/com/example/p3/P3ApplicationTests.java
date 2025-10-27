b09ad9ae36e71ca21102c26478c552fc530d3bd0
Katrine123 <113664220+Katrine123@users.noreply.github.com>
2025-10-27 15:14:05 +0100

    it works
    finally
    
    department test

--- src/test/java/com/example/p3/P3ApplicationTests.java
+++ src/test/java/com/example/p3/P3ApplicationTests.java
@@ -1,7 +1,9 @@
 package com.example.p3;
 
+import aj.org.objectweb.asm.TypeReference;
 import com.example.p3.dtos.UserDto;
 import com.example.p3.model.User;
+import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
@@ -19,6 +21,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -41,13 +44,33 @@ class P3ApplicationTests {
 
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode root = mapper.readTree(response.getBody());
+        JsonNode root = mapper.readValue(response.getBody(), JsonNode.class);
         String id = root.path("id").toString();
 
         String expectedJson = "15";
 
         Assertions.assertEquals(HttpStatus.OK,response.getStatusCode());
         Assertions.assertEquals(expectedJson,id);
+    }
+
+    //Tests for initials, email and name work the same
+
+    @Test
+    void testGetUserByDepartment() throws Exception {
+        String url = "http://localhost:8080/getUsers/department/DEVOPS";
+
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode[] root = mapper.readValue(response.getBody(), JsonNode[].class);
+        Assertions.assertEquals(HttpStatus.OK,response.getStatusCode());
+        String expectedJson = "\"DEVOPS\"";
+        for(JsonNode node : root) {
+            Assertions.assertEquals(expectedJson,node.path("department").toString());
+
+        }
+
+
+
     }
 
 //    @Autowired
