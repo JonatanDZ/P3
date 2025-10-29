3d849d7e80d90d8be4ebfbe48988d928b775f5c1
Nicolai-Schultz <nicoschultz42@gmail.com>
2025-10-29 14:22:18 +0100

    Revert "indent fix"
    This reverts commit 80e99246a8bed3a2d4d64c92a383eff3782b9120.

--- src/main/resources/db/migration/V1__init_migration.sql
+++ src/main/resources/db/migration/V1__init_migration.sql
@@ -1,72 +1,72 @@
-CREATE TABLE `department`(
-    `department_id`   INT,
-    `department_name` VARCHAR(255),
-    `is_dev`          BOOL,
-    PRIMARY KEY (`department_id`)
+CREATE TABLE `department` (
+                              `department_id` INT,
+                              `department_name` VARCHAR(255),
+                              `is_dev` BOOL,
+                              PRIMARY KEY (`department_id`)
 );
 
-CREATE TABLE `users`(
-    `user_id`       INT,
-    `name`          VARCHAR(255),
-    `initials`      VARCHAR(255),
-    `department_id` INT,
-    `is_admin`      BOOL,
-    PRIMARY KEY (`user_id`),
-    FOREIGN KEY (`department_id`)
-        REFERENCES `department` (`department_id`)
+CREATE TABLE `users` (
+                         `user_id` INT,
+                         `name` VARCHAR(255),
+                         `initials` VARCHAR(255),
+                         `department_id` INT,
+                         `is_admin` BOOL,
+                         PRIMARY KEY (`user_id`),
+                         FOREIGN KEY (`department_id`)
+                             REFERENCES `department`(`department_id`)
 );
 
-CREATE TABLE `jurisdiction`(
-    `jurisdiction_id`   INT,
-    `jurisdiction_name` VARCHAR(255),
-    PRIMARY KEY (`jurisdiction_id`)
+CREATE TABLE `jurisdiction` (
+                                `jurisdiction_id` INT,
+                                `jurisdiction_name` VARCHAR(255),
+                                PRIMARY KEY (`jurisdiction_id`)
 );
 
-CREATE TABLE `tool`(
-    `tool_id`    INT,
-    `name`       VARCHAR(255),
-    `url`        VARCHAR(255),
-    `is_dynamic` BOOL,
-    PRIMARY KEY (`tool_id`)
+CREATE TABLE `tool` (
+                        `tool_id` INT,
+                        `name` VARCHAR(255),
+                        `url` VARCHAR(255),
+                        `is_dynamic` BOOL,
+                        PRIMARY KEY (`tool_id`)
 );
 
-CREATE TABLE `department_tool`(
-    `department` INT,
-    `tool_id`    INT,
-    FOREIGN KEY (`department`)
-        REFERENCES `department` (`department_id`),
-    FOREIGN KEY (`tool_id`)
-        REFERENCES `tool` (`tool_id`)
+CREATE TABLE `department_tool` (
+                                   `department` INT,
+                                   `tool_id` INT,
+                                   FOREIGN KEY (`department`)
+                                       REFERENCES `department`(`department_id`),
+                                   FOREIGN KEY (`tool_id`)
+                                       REFERENCES `tool`(`tool_id`)
 );
 
-CREATE TABLE `stage`(
-    `stage_id` INT,
-    `name`     VARCHAR(255),
-    PRIMARY KEY (`stage_id`)
+CREATE TABLE `stage` (
+                         `stage_id` INT,
+                         `name` VARCHAR(255),
+                         PRIMARY KEY (`stage_id`)
 );
 
-CREATE TABLE `stage_tools`(
-    `tool_id`  INT,
-    `stage_id` INT,
-    FOREIGN KEY (`stage_id`)
-        REFERENCES `stage` (`stage_id`),
-    FOREIGN KEY (`tool_id`)
-        REFERENCES `tool` (`tool_id`)
+CREATE TABLE `stage_tools` (
+                               `tool_id` INT,
+                               `stage_id` INT,
+                               FOREIGN KEY (`stage_id`)
+                                   REFERENCES `stage`(`stage_id`),
+                               FOREIGN KEY (`tool_id`)
+                                   REFERENCES `tool`(`tool_id`)
 );
 
-CREATE TABLE `jurisdiction_tools`(
-    `tool_id`         INT,
-    `jurisdiction_id` INT,
-    FOREIGN KEY (`tool_id`)
-        REFERENCES `tool` (`tool_id`),
-    FOREIGN KEY (`jurisdiction_id`)
-        REFERENCES `jurisdiction` (`jurisdiction_id`)
+CREATE TABLE `jurisdiction_tools` (
+                                      `tool_id` INT,
+                                      `jurisdiction_id` INT,
+                                      FOREIGN KEY (`tool_id`)
+                                          REFERENCES `tool`(`tool_id`),
+                                      FOREIGN KEY (`jurisdiction_id`)
+                                          REFERENCES `jurisdiction`(`jurisdiction_id`)
 );
 
-CREATE TABLE favorite_tools(
-    user_id INT NOT NULL,
-    tool_id INT NOT NULL,
-    PRIMARY KEY (user_id, tool_id),
-    FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE,
-    FOREIGN KEY (tool_id) REFERENCES tool (tool_id) ON DELETE CASCADE
+CREATE TABLE favorite_tools (
+                                user_id INT NOT NULL,
+                                tool_id INT NOT NULL,
+                                PRIMARY KEY (user_id, tool_id),
+                                FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+                                FOREIGN KEY (tool_id) REFERENCES tool(tool_id) ON DELETE CASCADE
 );
