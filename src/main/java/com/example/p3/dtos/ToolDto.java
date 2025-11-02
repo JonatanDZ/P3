ac794a742f69ce46b5375f4672bf33fe870a3c9f
8ruun <mbja24@student.aau.dk>
2025-11-02 14:58:26 +0100

    Update ToolDto.java
    Jeg har gjort, så at du kun forholder sig til id'et i stedet for objekter i objekter i objekter

--- src/main/java/com/example/p3/dtos/ToolDto.java
+++ src/main/java/com/example/p3/dtos/ToolDto.java
@@ -4,6 +4,8 @@ import com.example.p3.entities.Department;
 import com.example.p3.entities.Tool;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 // data transfer objects, to JSON
@@ -14,7 +16,8 @@ public class ToolDto {
     private String name;
     private String url;
     private Boolean isDynamic;
-    private Set<Department> departments;
+    //private Set<Department> departments;
+    private ArrayList<Integer> departments;
 
     /*private String[] tags;
     // there can be multiple departments to a Tool
@@ -31,7 +34,15 @@ public class ToolDto {
         this.url = t.getUrl();
         this.isDynamic = t.getIsDynamic();
         //this.tags = t.getTags();
-        this.departments = t.getDepartments();
+        this.departments = new ArrayList<>();
+        int departmentID;
+        List<Department> departmenList = t.getDepartments().stream().toList();
+        for (int i = 0; i < departmenList.size(); i++) {
+            departmentID = departmenList.get(i).getId();
+            System.out.println(departmentID);
+            this.departments.add(departmentID);
+        }
+        //this.departments = t.getDepartments();
         //this.stages = t.getStages();
         //this.jurisdictions = t.getJurisdictions();
     }
