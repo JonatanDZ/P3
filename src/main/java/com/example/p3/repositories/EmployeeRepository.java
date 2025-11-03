f62188dfb598a602b618bb7316647c102d3c01c5
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2025-11-03 10:40:49 +0100

    comments

--- src/main/java/com/example/p3/repositories/EmployeeRepository.java
+++ src/main/java/com/example/p3/repositories/EmployeeRepository.java
@@ -8,6 +8,9 @@ import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    // Queries methods such as findAll() is standard in JpaRepository.
+    // findByIntitials is a custome query to find initials, sql would be smt like:
+    // SELECT * FROM employees WHERE initials = ?;
     // Finds employees by initials (case-sensitive)
     List<Employee> findByInitials(String initials);
 
