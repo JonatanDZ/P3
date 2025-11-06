2cc41b93380f8b0293e74e2f46c4257281c28c7b
Nikolaidk3 <hp82fk@student.aau.dk>
2025-11-06 14:00:05 +0100

    tilføjet test classes og rykket rundt for læsbarhed

---
+++ src/test/java/com/example/p3/EmployeeRepositoryTest.java
@@ -0,0 +1,22 @@
+package com.example.p3;
+
+import com.example.p3.entities.Employee;
+import com.example.p3.repositories.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class EmployeeRepositoryTest {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    public void testFindByInitials(){
+
+    }
+}
