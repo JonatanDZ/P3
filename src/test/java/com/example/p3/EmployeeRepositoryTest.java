c6418aaf749c8ae60f7787acc6ac34a9dd42713a
Nikolaidk3 <hp82fk@student.aau.dk>
2025-11-10 09:28:31 +0100

    små ændringer

--- src/test/java/com/example/p3/EmployeeRepositoryTest.java
+++ src/test/java/com/example/p3/EmployeeRepositoryTest.java
@@ -1,18 +1,14 @@
 package com.example.p3;
 
-import com.example.p3.entities.Department;
 import com.example.p3.entities.Employee;
 import com.example.p3.repositories.EmployeeRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
