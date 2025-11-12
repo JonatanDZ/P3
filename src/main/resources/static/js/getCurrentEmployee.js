38b115444fa10e54a5396a71326029c7384b8476
Nicolai-Schultz <nicoschultz42@gmail.com>
2025-11-12 14:03:39 +0100

    comments pushed

--- src/main/resources/static/js/getCurrentEmployee.js
+++ src/main/resources/static/js/getCurrentEmployee.js
@@ -1,7 +1,11 @@
 export async function getCurrentEmployee(initials = "PEDO") {
+    // hardcoded by having a constant argument of employee with initials: "PEDO"
+    // fetching from employee endpoint. Returns a JSON object of the employee
     const response = await fetch(`/employee/initials/${initials}`);
+    // throws if  other response than 200
     if (!response.ok) {
         throw new Error(`HTTP ${response.status}`);
     }
+    // returns entire json object. It is expected to access attributes by dot notation after getting  the entire user object.
     return await response.json();
 }
