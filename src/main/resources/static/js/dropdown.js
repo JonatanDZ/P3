5939662b51a3f4f6574a26a5c550b9c2831ae0bf
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2025-11-14 11:38:38 +0100

    Added cards to dropdown

--- src/main/resources/static/js/dropdown.js
+++ src/main/resources/static/js/dropdown.js
@@ -7,8 +7,73 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-// get deparment notifications
-// if user(hardcoded one) match a deparment with notifactions (pending tool).
-// then add notifications to bell card
+document.addEventListener("DOMContentLoaded", async () => {
+    const pendingToolList = await getDepartmentsPendingTools()
 
-// make approve and deny function
+    if (pendingToolList.length < 1) {
+        // notify bell
+        bellNotifier(pendingToolList.length);
+
+        // make dropdown cards
+        createDropdownCards(pendingToolList);
+    }
+
+    // after DOMContentLoaded is run. its runs every 60 seconds to check for updates.
+    setInterval(async () => {
+        if (pendingToolList.length < 1) {
+            // notify bell
+            bellNotifier(pendingToolList.length);
+
+            // make dropdown cards
+            createDropdownCards(pendingToolList);
+        }
+    }, 6000);
+});
+
+// skal laves så den kun giver liste med pending tools
+async function getDepartmentsPendingTools(department) {
+    const response = await fetch(`/employee/department/{department}`);
+
+    if (!response.ok) {
+        throw new Error(`HTTP ${response.status}`);
+    }
+
+    return await response.json();
+}
+
+function bellNotifier(pendingToolListLength) {
+    // bases on the pendingToolListLength it should make a bell icon that shows the size of the list
+}
+
+function createDropdownCards(pendingToolList) {
+    const containerDropdownId = document.getElementById("dropdownIndividualItem");
+
+    pendingToolList.forEach(pendingToolList => {
+        const card = createDropdownCard(pendingToolList);
+        containerDropdownId.appendChild(card);
+    });
+}
+
+function createDropdownCard(pendingTool) {
+    const pendingToolCard = document.createElement("a");
+    pendingToolCard.href = pendingTool.url;
+    pendingToolCard.classList.add("dropdownItemText");
+
+    const label = document.createElement("span")
+    label.classList.add("dropdownItemText");
+    label.textContent = pendingTool.text;
+
+    const approveBtn = document.createElement("button");
+    approveBtn.classList.add("aprbtn");
+    approveBtn.textContent = "✅";
+
+    const denyBtn = document.createElement("button");
+    denyBtn.classList.add("denybtn");
+    denyBtn.textContent = "🚫";
+
+    pendingToolCard.appendChild(label);
+    pendingToolCard.appendChild(approveBtn);
+    pendingToolCard.appendChild(denyBtn);
+
+    return pendingToolCard;
+}
