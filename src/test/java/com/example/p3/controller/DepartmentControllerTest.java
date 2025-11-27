ade14727b999e7ed2f0f7d677923a2554f8af7b4
Asta Krogsgaard Jensen <asta@krogsgaarden.dk>
2025-11-27 10:15:47 +0100

    .

---
+++ src/test/java/com/example/p3/controller/DepartmentControllerTest.java
@@ -0,0 +1,9 @@
+package com.example.p3.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+@WebMvcTest(EmployeeController.class)
+public class DepartmentControllerTest {
+
+
+}
---
+++ src/test/java/com/example/p3/repository/DepartmentRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.p3.repository;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class DepartmentRepositoryTest {
+
+
+
+}
