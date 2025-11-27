c026510637e5e0407bb8126ed2d28ee87e2881d2
Asta Krogsgaard Jensen <asta@krogsgaarden.dk>
2025-11-27 10:37:00 +0100

    .

--- src/test/java/com/example/p3/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/example/p3/repository/DepartmentRepositoryTest.java
@@ -2,8 +2,10 @@ package com.example.p3.repository;
 
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DepartmentRepositoryTest {
 
