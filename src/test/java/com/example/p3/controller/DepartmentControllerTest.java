c4374a2bfbd1c02173641c45e9f06b0a786608eb
Asta Krogsgaard Jensen <asta@krogsgaarden.dk>
2025-11-28 09:16:14 +0100

    DepartmentRepTestDone

--- src/test/java/com/example/p3/controller/DepartmentControllerTest.java
+++ src/test/java/com/example/p3/controller/DepartmentControllerTest.java
@@ -9,13 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -45,12 +42,13 @@ public class DepartmentControllerTest {
     }
 
     @Test
-    public void testGetDepartmentById() throws Exception {
+    public void testGetAllDepartment() throws Exception {
         //Mock departments constructed
         Department department1 = DepartmentConstructor(1, "Devops", true);
         Department department2 = DepartmentConstructor(2, "HR", false);
         Department department3 = DepartmentConstructor(3, "Promotions", true);
 
+        //The department are inserted into a list
         List<Department> departments = List.of(department1, department2, department3);
 
         //Checks if getAllDepartments returns all the correct values from the endpoint
@@ -60,13 +58,13 @@ public class DepartmentControllerTest {
                 //Since it is a list the index is specified for the values
                 .andExpect(jsonPath("[0].id").value(1))
                 .andExpect(jsonPath("[0].name").value("Devops"))
-                .andExpect(jsonPath("[0]._dev").value(true))
+                .andExpect(jsonPath("[0]._dev").value(true)) //IDK why it is returned as "_dev" ad not "is_dev"
                 .andExpect(jsonPath("[1].id").value(2))
                 .andExpect(jsonPath("[1].name").value("HR"))
-                .andExpect(jsonPath("[1]._dev").value(false))
+                .andExpect(jsonPath("[1]._dev").value(false)) //IDK why it is returned as "_dev" ad not "is_dev"
                 .andExpect(jsonPath("[2].id").value(3))
                 .andExpect(jsonPath("[2].name").value("Promotions"))
-                .andExpect(jsonPath("[2]._dev").value(true));
+                .andExpect(jsonPath("[2]._dev").value(true)); //IDK why it is returned as "_dev" ad not "is_dev"
 
     }
 }
--- src/test/java/com/example/p3/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/example/p3/repository/DepartmentRepositoryTest.java
@@ -1,14 +1,63 @@
 package com.example.p3.repository;
 
+import com.example.p3.entities.Department;
+import com.example.p3.entities.Employee;
+import com.example.p3.repositories.DepartmentRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class DepartmentRepositoryTest {
+public class DepartmentRepositoryTest extends RepositoryGlobalMethods{
+    @Autowired
+    private DepartmentRepository departmentRepositoryTest;
 
+    @Test
+    public void testDepartmentById(){
+        Department d1 = new Department(null, "Frontend", true); //id must be null for JPA to generate it
+        //to ensure the correct id is used it will be saved and store in a variable "saved" which is then referenced
+        Department saved = departmentRepositoryTest.save(d1);
 
+        Optional<Department> department = departmentRepositoryTest.findById(saved.getId());
+        assertNotNull(department);
+        assertTrue(department.isPresent());
+    }
 
+    @Test
+    public void testDepartmentByIdNotPresent(){
+        Optional<Department> department = departmentRepositoryTest.findById(10);
+        //Assert true that the employee returned is empty, as there is no employee with initials KOLO
+        assertTrue(department.isEmpty(), "Department not found");
+    }
+
+    @Test
+    public void testGetAllDepartment(){ //Check the amount of saved departments are in the repository
+        //Mock departments are constructed
+        Department d1 = new Department(null, "Frontend", true); //id must be null for JPA to generate it
+        Department d2 = new Department(null, "Legal", false);
+        Department d3 = new Department(null, "HR", false);
+        Department d4 = new Department(null, "Devops", true);
+        Department d5 = new Department(null, "Payments", true);
+
+        //Mock departments are saved in the repository
+        departmentRepositoryTest.save(d1);
+        departmentRepositoryTest.save(d2);
+        departmentRepositoryTest.save(d3);
+        departmentRepositoryTest.save(d4);
+        departmentRepositoryTest.save(d5);
+
+        //The departments are put into a list
+        List<Department> department = departmentRepositoryTest.findAll();
+        assertNotNull(department);
+        assertEquals(5, department.size());
+    }
 }
