3cd2fdea0ff26546783bf82b7a230c3baac26dcc
Peter Bødstrup <HO77IY@student.aau.dk>
2025-12-04 10:14:35 +0100

    Update JurisdictionController.java

--- src/main/java/com/example/p3/controller/JurisdictionController.java
+++ src/main/java/com/example/p3/controller/JurisdictionController.java
@@ -4,7 +4,6 @@ import com.example.p3.dtos.JurisdictionDto;
 import com.example.p3.service.JurisdictionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,13 +27,3 @@ public class JurisdictionController {
     }
 
 }
-
-/*
-    @GetMapping("/name/{name}")
-    public ResponseEntity<JurisdictionDto> getJurisdictionsByName(@PathVariable String name){
-        return JurisdictionService.getByName(name)
-                .map(JurisdictionDto::new)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
- */
