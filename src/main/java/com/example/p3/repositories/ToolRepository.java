cd333e843b4f6f04756235a73746b82ba836ace9
Peter Bødstrup <HO77IY@student.aau.dk>
2025-12-04 10:09:03 +0100

    Update ToolRepository.java

--- src/main/java/com/example/p3/repositories/ToolRepository.java
+++ src/main/java/com/example/p3/repositories/ToolRepository.java
@@ -105,37 +105,7 @@ public interface ToolRepository extends JpaRepository<Tool, Integer> {
     
     
 """)
-
-
-    /*
-    SELECT * FROM tool t
-    JOIN tool_jurisdiction tj ON t.id = tj.tool_id
-    JOIN jurisdiction j ON tj.jurisdiction_id = j.id
-    WHERE j.name = jurisdictionName;
-     */
-    //List<Tool> findByJurisdictions_JurisdictionName(String jurisdictionName);
-
-    /*
-    SELECT * FROM tool t
-    JOIN tool_stage ts ON t.id = ts.tool_id
-    JOIN stage s ON ts.stage_id = s.id
-    WHERE s.name = stageName;
-     */
-    //List<Tool> findByStages_Name(String stageName);
-
-    /*
-    SELECT * FROM tool t
-    JOIN department_tool dt ON t.id = dt.tool_id
-    JOIN department d ON dt.department_id = d.id
-    JOIN tool_jurisdiction tj ON t.id = tj.tool_id
-    JOIN jurisdiction j ON tj.jurisdiction_id = j.id
-    JOIN tool_stage ts ON t.id = ts.tool_id
-    JOIN stage s ON ts.stage_id = s.id
-    WHERE d.name = departmentName
-    AND j.name = jurisdictionName
-    AND s.name = stageName;
-    */
-
+    
 
     List<Tool> findByDepartments_NameAndJurisdictions_NameAndStages_Name(String DepartmentName,
                                                                          String JurisdictionName,
