fccd3799416a3dc697cbc10020b616700f6bde3f
Peter Bødstrup <HO77IY@student.aau.dk>
2025-12-04 10:10:39 +0100

    Update JurisdictionRepository.java

--- src/main/java/com/example/p3/repositories/JurisdictionRepository.java
+++ src/main/java/com/example/p3/repositories/JurisdictionRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface JurisdictionRepository extends JpaRepository<Jurisdiction, Integer> {
-    //Optional<Jurisdiction> findByName(String name);
 }
