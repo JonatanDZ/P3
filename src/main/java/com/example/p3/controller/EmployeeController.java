2eee4df583f1dfe542ada5f4468a355bc07a4644
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2025-12-11 14:21:15 +0100

    Update EmployeeController.java

--- src/main/java/com/example/p3/controller/EmployeeController.java
+++ src/main/java/com/example/p3/controller/EmployeeController.java
@@ -3,15 +3,21 @@ package com.example.p3.controller;
 import com.example.p3.dtos.EmployeeDto;
 import com.example.p3.entities.Employee;
 import com.example.p3.service.EmployeeService;
+
 import io.swagger.v3.oas.annotations.Operation;
+
 import lombok.AllArgsConstructor;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
+// Makes "/employee" always be a part of the endpoint call
 @RequestMapping("/employee")
 public class EmployeeController {
+
+    // Lombok generates the constructor.
     private final EmployeeService employeeService;
 
     //This is an annotation used for swagger. It allows for understanding the endpoint at a quick glance
@@ -19,11 +25,13 @@ public class EmployeeController {
             summary = "Get employee by initials",
             description = "Looks up an employee based on their initials. Initials must be 2–4 letters, including Danish characters (Æ, Ø, Å)."
     )
+
     // "{initials}" is the path variable the endpoint will take
     @GetMapping("/initials/{initials}")
     public ResponseEntity<EmployeeDto> getEmployeeByInitials(@PathVariable String initials) {
         //Reject invalid input before calling the service
         //Check if the given initials do not match the pattern only letters from A-Å upper and lower case and only 2-4 characters
+        // made with regex
         if (!initials.matches("^[A-Za-zÆØÅæøå]{2,4}$")){
             return ResponseEntity.badRequest().build();
         }
