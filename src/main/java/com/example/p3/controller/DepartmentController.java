3da85e9a0831b70e544374a1b76544bece961419
JonatanDZ <99177934+JonatanDZ@users.noreply.github.com>
2025-12-11 13:33:43 +0100

    Added comments: Department Controller

--- src/main/java/com/example/p3/controller/DepartmentController.java
+++ src/main/java/com/example/p3/controller/DepartmentController.java
@@ -2,7 +2,9 @@ package com.example.p3.controller;
 
 import com.example.p3.dtos.DepartmentDto;
 import com.example.p3.service.DepartmentService;
+
 import io.swagger.v3.oas.annotations.Operation;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 // This is the API http/rest controller
 @RestController
 // Makes "/departments" always being a part of the endpoint call
 @RequestMapping("/departments")
 public class DepartmentController {
 
+    // DepartmentService must be assigned once in the constructor and can't be changed later.
+    // This is the object that talks to the service layer.
     private final DepartmentService departmentService;
 
+    // TODO: We should remove this and use lombok like every other controller.
+    // The DepartmentControllers constructor.
     public DepartmentController(DepartmentService departmentService) {
         this.departmentService = departmentService;
     }
@@ -28,19 +33,20 @@ public class DepartmentController {
             summary = "Gets a list of all departments",
             description = "Returns every department in the database in the form of a list."
     )
+
     // The endpoint is empty and will run if "/departments" are called
     @GetMapping("")
     public ResponseEntity<List<DepartmentDto>> getAllDepartments(){
         // Calls service to get all departments
+        // Fetch all department entities and convert them to DTOs
         List<DepartmentDto> list = departmentService.getAllDepartments()
-                // Turns it into a stream to be processed with the streams API
+                // Converts the list returned by the service into a stream so we can use the Streams API (map, filter, etc.).
                 .stream()
-                // For each department in the stream, a new departmentDto is created
+                // For each department(department entity) in the stream, a new departmentDto is created with the DepartmentDto constructor.
                 .map(DepartmentDto::new)
                 // Collects the mapped stream and puts it into List<DepartmentDto>
                 .toList();
+        // Returns a list of departmentDTOs wrapped in a ResponseEntity to control HTTP status, headers, etc.
         return ResponseEntity.ok(list);
     }
-
-
 }
